63e7db5dbd2d859dae5a90a79d7fb70273c61703
Kay Hennig <KayHennig98@gmail.com>
2023-10-05 12:13:08 +0200

    base

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+// Type definitions for ZAFClient 2.0 (https://developer.zendesk.com/api-reference/apps/apps-core-api/client_api/)
+// Project: https://developer.zendesk.com/api-reference/apps/apps-core-api/client_api/
+// Definitions by: Kay Hennig <https://github.com/TheFusion21>
+// Definitions: https://github.com/TheFusion21/types-ZAFClient
+
+/**
+ * Initializes a client instance
+ */
+declare var ZAFClient: {
+  init(): ZAFClientInstance;
+}
+
+/**
+ * The client instance
+ */
+interface ZAFClientInstance {
+
+}
